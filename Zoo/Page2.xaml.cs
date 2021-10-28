df3f1df84ae6d8b4cc30fb168dde4367c8cc204d
Мясников Михаил Николаевич <201913@mck-ktits.ru>
2021-10-28 20:42:06 +0300

    Добавьте файлы проекта.

---
+++ Zoo/Page2.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zoo
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    
+    public partial class Page2 : Page
+    {
+        public static ObservableCollection<User> users { get; set; }
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void loginClick(object sender, RoutedEventArgs e)
+        {
+            users = new ObservableCollection<User>(DBConnect.connection.User.ToList());
+            var z = users.Where(a => a.Login == txtLogin.Text && a.password == txtPassword.Password).FirstOrDefault();
+            if (z != null)
+            {
+                MessageBox.Show(z.Name);
+            }
+            else
+            {
+                MessageBox.Show("Invalid User", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void registerClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageReg());
+        }
+    }
+}
---
+++ Zoo/PageReg.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zoo
+{
+    /// <summary>
+    /// Логика взаимодействия для PageReg.xaml
+    /// </summary>
+    public partial class PageReg : Page
+    {
+        public PageReg()
+        {
+            InitializeComponent();
+        }
+
+        private void BackClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var u = new User();
+            u.Name = txtNAme.Text;
+            u.Login = txtLogin.Text;
+            u.password = txtPassword.Password;
+            DBConnect.connection.User.Add(u);
+            DBConnect.connection.SaveChanges();
+            MessageBox.Show("All ok");
+            NavigationService.GoBack();
+        }
+    }
+}
